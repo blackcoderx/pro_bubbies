4a39a559ab0f0e6879360694fa2eca38ceffb804
Emmanuel Ziggah <116467025+blackcoderx@users.noreply.github.com>
2024-04-15 16:21:55 +0000

    Update settings.py

--- pro_bubbies/settings.py
+++ pro_bubbies/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'pro_bubbies.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.sqlite3",
+        'HOST': "aws-0-sa-east-1.pooler.supabase.com",
+        'NAME': "postgres",
+        'USER': "postgres.dookaeeigdasbwdnloji",
+        'PASSWORD': "S+74GgNzW?qsyT&",
+        'PORT': "5432",
+
     }
 }
 
